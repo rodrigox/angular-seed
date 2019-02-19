7723ff7ce1015c3c4db755ce6b270c81611046a6
Rodrigo Oliveira <rodrigo.b.oliveira@accenture.com>
2019-02-19 10:17:25 -0300

    angular forms lessons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    HomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
